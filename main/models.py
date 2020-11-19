bfd7eb008526751f13ee3e5cdad419472e64de25
saleembilal-S <67510015+saleembilal-S@users.noreply.github.com>
2020-11-19 16:24:12 +0200

    Added last feature, Take a video from user then display it

--- main/models.py
+++ main/models.py
@@ -8,6 +8,17 @@ class CompanyInfo(models.Model):
     email_of_company = models.CharField(max_length=30, unique=True)
     password_of_company=models.CharField(max_length=20)
     activation_code=models.CharField(max_length=30)
+    number_of_camera = models.IntegerField(default=0)
 
     def __str__(self):
         return self.email_of_company
+
+
+class CamerasInfo(models.Model):
+    name_of_camera = models.CharField(max_length=30)
+    ip_of_camera = models.CharField(max_length=50)
+    company_info_id = models.ForeignKey(CompanyInfo, on_delete=models.CASCADE)
+    is_running = models.BooleanField()
+
+    def __str__(self):
+        return self.name_of_camera
--- main/urls.py
+++ main/urls.py
@@ -11,8 +11,9 @@ urlpatterns = [
     path('401/',views.page401,name='401'),
     path('logout/',views.logout,name='logout'),
     path('addnewcamera/',views.addnewcamera,name='addnewcamera'),
-    path('video_result/', views.video_result, name='video_result'),
-    path('camera_result/', views.camera_result, name='camera_result'),
+    # path('video_result/', views.video_result, name='video_result'),
+    path('ip_result/', views.ip_result, name='ip_result'),
+    # path('camera_result/', views.camera_result, name='camera_result'),
 
 
 
